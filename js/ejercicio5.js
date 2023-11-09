17eb7f03ba57da5257da7f2ff7ed1a34a3bf905b
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-11-09 01:44:59 -0300

    Se resuelve el ejercicio 5

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,1 +1,16 @@
+//5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
 
+var texto = prompt("Ingrese un texto");
+var resultado = analizarTexto(texto);
+
+function analizarTexto(texto) {
+  if (texto === texto.toUpperCase()) {
+    return 'El texto está formado solo por mayúsculas.';
+  } else if (texto === texto.toLowerCase()) {
+    return 'El texto está formado solo por minúsculas.';
+  } else {
+    return 'El texto está formado por una mezcla de mayúsculas y minúsculas.';
+  }
+}
+
+document.write(resultado);
